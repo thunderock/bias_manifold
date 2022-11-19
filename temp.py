bd28455e93b3cd077a8f05e0f3b25e9bc0c64082
Ashutosh Tiwari <ashutiwa@iu.edu>
2022-11-19 13:40:15 -0500

    started writing report

--- temp.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-class Test(object):
-    def func(self):
-        from utils.config_utils import _CONFIG_STORE
-
-        print("testing string: ", _CONFIG_STORE)
-
-np.save("/tmp/scores.npy", np.zeros(1))
--- test.py
+++
@@ -1,9 +0,0 @@
-from datasets import wikidataset
-from models import glove
-
-m = glove.Glove()
-file = 'http://www.cs.toronto.edu/~mebrunet/simplewiki-20171103-pages-articles-multistream.xml.bz2'
-
-lines = wikidataset.WikiDataset(file).lines
-
-m.fit(lines)
